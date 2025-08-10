816aabb4d14399cf8082269881206fffcb89f45f
Ridowan Sarder <ridwanzuraiz@gmail.com>
2025-08-10 17:48:50 +0600

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js"
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const port = process.env.PORT || 8000
+
+connectDB()
+.then(() => {
+  app.listen(port, () => {
+    console.log(`Server is listening at port ${port}`)
+  } )
+})
+.catch((error) => {
+  console.log("Database connection failed!", error);
+  
+})
